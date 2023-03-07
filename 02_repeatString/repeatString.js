30debb8aa493c399214c1321b2a0703034e38c61
Iago Cardoso <iagonc.dev@gmail.com>
2023-03-07 19:51:11 -0300

    Complete repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num >= 0) {
+    const word = string;
+    let repeat = ""
+    for ( let i = 0 ; i < num ; i++) {
+        repeat += word;
+    };
+    return repeat
+    }
+    else {
+     return 'ERROR'
+    }
+    
 };
 
 // Do not edit below this line
