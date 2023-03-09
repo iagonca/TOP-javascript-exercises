34452c05f4ac62ba0e607e32a347018a2d2f62a2
Iago Cardoso <iagonc.dev@gmail.com>
2023-03-09 18:25:52 -0300

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...arg) {
+    for (let i = 0; i < arg.length; i++) {
+        if (array.includes(arg[i]))
+        array.splice(array.indexOf(arg[i]), 1)
+    }
+    return array
 };
 
 // Do not edit below this line
