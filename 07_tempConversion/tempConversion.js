85e09cfbde0d1588969f018d545b664b56e0b7fa
Iago Cardoso <iagonc.dev@gmail.com>
2023-03-11 11:47:53 -0300

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  return Math.round((F-32) * (5/9) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  return Math.round((C*(9/5) + 32) * 10) / 10
 };
 
+convertToCelsius(32)
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
